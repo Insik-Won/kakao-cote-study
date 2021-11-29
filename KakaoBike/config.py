d3e627568ead0eb54389be82ad520c4cc937ed0e
MrWon <won020725@naver.com>
2021-11-29 22:13:22 +0900

    add KakaoBike/Pipfile

--- KakaoBike/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv(verbose=True)
-
-BASE_URL = os.getenv("BASE_URL")
-X_AUTH_TOKEN = os.getenv("X_AUTH_TOKEN")
--- KakaoBike/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-from config import BASE_URL, X_AUTH_TOKEN
-
-if __name__ == '__main__':
-  print(BASE_URL)
-  print(X_AUTH_TOKEN)
